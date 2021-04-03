3d5f24eb91575421867dc1a9e83a1b947ca3dd82
Flor Condori <condorihuamanif@gmail.com>
2021-04-03 02:15:00 -0500

    initial settings

---
+++ src/components/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+
+const HelloWorld = () => {
+    const name = 'Flor';
+    return(<h1>HelloWorld!! {name}</h1>);
+}
+export default HelloWorld;
